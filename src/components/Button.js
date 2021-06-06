bbd6550a4bab8de9c92e59dd19863447a9a30d80
suchetamahata <70646658+suchetamahata@users.noreply.github.com>
2021-06-06 19:02:59 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,22 +4,17 @@ import PropTypes from "prop-types";
 class Button extends Component{
     static propTypes={
             name: PropTypes.string,
-            orange: PropTypes.bool,
-            wide: PropTypes.bool,
+            //orange: PropTypes.bool,
+            //wide: PropTypes.bool,
             clickHandler: PropTypes.func,
         }
     handleClick = () => {
         this.props.clickHandler(this.props.name);
     };
     render() {
-        const className = [
-          "component-button",
-          this.props.orange ? "orange" : "",
-          this.props.wide ? "wide" : "",
-        ];
     
         return (
-          <div className={className.join(" ").trim()}>
+          <div >
             <button onClick={this.handleClick}>  {this.props.name}   </button>
           </div>
         );
